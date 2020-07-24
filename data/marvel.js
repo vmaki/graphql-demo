1eb8ae1d6311c38685bf93edf3e5af01b4acab38
Maki <lindon_1@163.com>
2020-07-24 11:33:27 +0800

    add

---
+++ data/marvel.js
@@ -0,0 +1,137 @@
+const Marvel = `
+schema {
+  query: Query
+  mutation: Mutation
+  subscription: Subscription
+}
+
+# 查询
+type Query {
+  hero(episode: Episode): Character
+  reviews(episode: Episode!): [Review]
+  search(text: String): [SearchResult]
+  character(id: ID!): Character
+  alien(id: ID!): Alien
+  human(id: ID!): Human
+  hi(name: String): String
+}
+
+# 更新
+type Mutation {
+  createReview(episode: Episode, review: ReviewInput!): Review
+}
+
+# 订阅
+type Subscription {
+  reviewAdded(episode: Episode): Review
+}
+
+# 剧集
+enum Episode {
+  Avengers # 复仇者联盟
+  XMen # X战警
+  GuardiansOfTheGalaxy # 银河护卫队
+
+  IronMan # 钢铁侠
+  CaptainAmerica # 美国队长
+  Thor # 雷神
+}
+
+# 角色的基本属性
+interface Character {
+  id: ID!
+  title: String!
+  name: String!
+  appearsIn: [Episode]!
+
+  friends: [Character]
+  friendsConnection(first: Int, after: ID): FriendsConnection!
+}
+
+# 时间单位
+enum GenderUnit {
+  Sex
+  Gender
+}
+
+# 人类
+type Human implements Character {
+  id: ID!
+  title: String!
+  name: String!
+  appearsIn: [Episode]!
+
+  friends: [Character]
+  friendsConnection(first: Int, after: ID): FriendsConnection!
+  
+  gender(unit: GenderUnit = Gender): String
+  height: Float
+}
+
+# 外星人
+type Alien implements Character {
+  id: ID!
+  title: String!
+  name: String!
+  appearsIn: [Episode]!
+
+  friends: [Character]
+  friendsConnection(first: Int, after: ID): FriendsConnection!
+
+  homePlanet: String
+}
+
+# A connection object for a character's friends
+type FriendsConnection {
+  # The total number of friends
+  totalCount: Int
+
+  # The edges for each of the character's friends.
+  edges: [FriendsEdge]
+
+  # A list of the friends, as a convenience when edges are not needed.
+  friends: [Character]
+
+  # Information for paginating this connection
+  pageInfo: PageInfo!
+}
+
+# An edge object for a character's friends
+type FriendsEdge {
+  # A cursor used for pagination
+  cursor: ID!
+
+  # The character represented by this friendship edge
+  node: Character
+}
+
+# Information for paginating this connection
+type PageInfo {
+  startCursor: ID
+  endCursor: ID
+  hasNextPage: Boolean!
+}
+
+# 评论
+type Review {
+  episode: Episode
+  stars: Int!
+  commentary: String
+}
+
+input ReviewInput {
+  stars: Int!
+  commentary: String
+  favorite_color: ColorInput
+}
+
+input ColorInput {
+  red: Int!
+  green: Int!
+  blue: Int!
+}
+
+union SearchResult = Human | Alien
+`;
+
+export const schemaString = Marvel
--- data/swapiSchema.js
+++ data/swapiSchema.js
@@ -19,205 +19,10 @@ import {
 import { makeExecutableSchema } from 'graphql-tools';
 
 import { PubSub, SubscriptionManager, withFilter } from 'graphql-subscriptions';
+import {schemaString} from './marvel';
 
 const pubsub = new PubSub();
 const ADDED_REVIEW_TOPIC = 'new_review';
-
-const schemaString = `
-schema {
-  query: Query
-  mutation: Mutation
-  subscription: Subscription
-}
-
-# The query type, represents all of the entry points into our object graph
-type Query {
-  hero(episode: Episode): Character
-  reviews(episode: Episode!): [Review]
-  search(text: String): [SearchResult]
-  character(id: ID!): Character
-  droid(id: ID!): Droid
-  human(id: ID!): Human
-  starship(id: ID!): Starship
-}
-
-# The mutation type, represents all updates we can make to our data
-type Mutation {
-  createReview(episode: Episode, review: ReviewInput!): Review
-}
-
-# The subscription type, represents all subscriptions we can make to our data
-type Subscription {
-  reviewAdded(episode: Episode): Review
-}
-
-# The episodes in the Star Wars trilogy
-enum Episode {
-  # Star Wars Episode IV: A New Hope, released in 1977.
-  NEWHOPE
-
-  # Star Wars Episode V: The Empire Strikes Back, released in 1980.
-  EMPIRE
-
-  # Star Wars Episode VI: Return of the Jedi, released in 1983.
-  JEDI
-}
-
-# A character from the Star Wars universe
-interface Character {
-  # The ID of the character
-  id: ID!
-
-  # The name of the character
-  name: String!
-
-  # The friends of the character, or an empty list if they have none
-  friends: [Character]
-
-  # The friends of the character exposed as a connection with edges
-  friendsConnection(first: Int, after: ID): FriendsConnection!
-
-  # The movies this character appears in
-  appearsIn: [Episode]!
-}
-
-# Units of height
-enum LengthUnit {
-  # The standard unit around the world
-  METER
-
-  # Primarily used in the United States
-  FOOT
-}
-
-# A humanoid creature from the Star Wars universe
-type Human implements Character {
-  # The ID of the human
-  id: ID!
-
-  # What this human calls themselves
-  name: String!
-
-  # The home planet of the human, or null if unknown
-  homePlanet: String
-
-  # Height in the preferred unit, default is meters
-  height(unit: LengthUnit = METER): Float
-
-  # Mass in kilograms, or null if unknown
-  mass: Float
-
-  # This human's friends, or an empty list if they have none
-  friends: [Character]
-
-  # The friends of the human exposed as a connection with edges
-  friendsConnection(first: Int, after: ID): FriendsConnection!
-
-  # The movies this human appears in
-  appearsIn: [Episode]!
-
-  # A list of starships this person has piloted, or an empty list if none
-  starships: [Starship]
-}
-
-# An autonomous mechanical character in the Star Wars universe
-type Droid implements Character {
-  # The ID of the droid
-  id: ID!
-
-  # What others call this droid
-  name: String!
-
-  # This droid's friends, or an empty list if they have none
-  friends: [Character]
-
-  # The friends of the droid exposed as a connection with edges
-  friendsConnection(first: Int, after: ID): FriendsConnection!
-
-  # The movies this droid appears in
-  appearsIn: [Episode]!
-
-  # This droid's primary function
-  primaryFunction: String
-}
-
-# A connection object for a character's friends
-type FriendsConnection {
-  # The total number of friends
-  totalCount: Int
-
-  # The edges for each of the character's friends.
-  edges: [FriendsEdge]
-
-  # A list of the friends, as a convenience when edges are not needed.
-  friends: [Character]
-
-  # Information for paginating this connection
-  pageInfo: PageInfo!
-}
-
-# An edge object for a character's friends
-type FriendsEdge {
-  # A cursor used for pagination
-  cursor: ID!
-
-  # The character represented by this friendship edge
-  node: Character
-}
-
-# Information for paginating this connection
-type PageInfo {
-  startCursor: ID
-  endCursor: ID
-  hasNextPage: Boolean!
-}
-
-# Represents a review for a movie
-type Review {
-  # The movie
-  episode: Episode
-
-  # The number of stars this review gave, 1-5
-  stars: Int!
-
-  # Comment about the movie
-  commentary: String
-}
-
-# The input object sent when someone is creating a new review
-input ReviewInput {
-  # 0-5 stars
-  stars: Int!
-
-  # Comment about the movie, optional
-  commentary: String
-
-  # Favorite color, optional
-  favorite_color: ColorInput
-}
-
-# The input object sent when passing in a color
-input ColorInput {
-  red: Int!
-  green: Int!
-  blue: Int!
-}
-
-type Starship {
-  # The ID of the starship
-  id: ID!
-
-  # The name of the starship
-  name: String!
-
-  # Length of the starship, along the longest axis
-  length(unit: LengthUnit = METER): Float
-
-  coordinates: [[Float!]!]
-}
-
-union SearchResult = Human | Droid | Starship
-`;
 
 /**
  * This defines a basic set of data for our Star Wars Schema.
@@ -229,53 +34,45 @@ union SearchResult = Human | Droid | Starship
 
 const humans = [
   {
-    id: '1000',
-    name: 'Luke Skywalker',
-    friends: [ '1002', '1003', '2000', '2001' ],
-    appearsIn: [ 'NEWHOPE', 'EMPIRE', 'JEDI' ],
-    homePlanet: 'Tatooine',
-    height: 1.72,
-    mass: 77,
-    starships: [ '3001', '3003' ],
+    id: '1000', title: '美国队长', name: '斯蒂夫·罗杰斯', gender: '先生', height: 188, 
+    friends: [ '1001','1002','1003','2001' ], appearsIn: [ 'CaptainAmerica', 'Avengers' ],
   },
   {
-    id: '1001',
-    name: 'Darth Vader',
-    friends: [ '1004' ],
-    appearsIn: [ 'NEWHOPE', 'EMPIRE', 'JEDI' ],
-    homePlanet: 'Tatooine',
-    height: 2.02,
-    mass: 136,
-    starships: [ '3002' ],
+    id: '1001', title: '钢铁侠', name: ' 托尼·斯塔克', gender: '先生', height: 185,
+    friends: [ '1000','1002','1007','2001' ], appearsIn: [ 'IronMan', 'Avengers', 'CaptainAmerica' ],
   },
   {
-    id: '1002',
-    name: 'Han Solo',
-    friends: [ '1000', '1003', '2001' ],
-    appearsIn: [ 'NEWHOPE', 'EMPIRE', 'JEDI' ],
-    height: 1.8,
-    mass: 80,
-    starships: [ '3000', '3003' ],
+    id: '1002', title: '黑寡妇', name: '娜塔莎·罗曼诺夫', gender: '女士', height: null,
+    friends: [ '1000','1001','2001' ], appearsIn: [ 'Avengers' ],
   },
   {
-    id: '1003',
-    name: 'Leia Organa',
-    friends: [ '1000', '1002', '2000', '2001' ],
-    appearsIn: [ 'NEWHOPE', 'EMPIRE', 'JEDI' ],
-    homePlanet: 'Alderaan',
-    height: 1.5,
-    mass: 49,
-    starships: [],
+    id: '1003', title: '金刚狼', name: '罗根', gender: '先生', height: 166,
+    friends: [ '1000','1005' ], appearsIn: [ 'Wolverine','XMen' ],
   },
   {
-    id: '1004',
-    name: 'Wilhuff Tarkin',
-    friends: [ '1001' ],
-    appearsIn: [ 'NEWHOPE' ],
-    height: 1.8,
-    mass: null,
-    starships: [],
+    id: '1004', title: '镭射眼', name: ' 斯科特·萨默斯', gender: '先生', height: 191,
+    friends: ['1005' ], appearsIn: [ 'XMen' ],
   },
+  {
+    id: '1005', title: '凤凰女', name: '琴·葛蕾', gender: '女士', height: 168,
+    friends: [ '1003','1004' ], appearsIn: [ 'XMen' ],
+  },
+  {
+    id: '1006', title: '星爵', name: '彼得·杰森·奎尔', gender: '先生', height: 192,
+    friends: [ '2002' ], appearsIn: [ 'GuardiansOfTheGalaxy', 'Avengers' ],
+  },
+  {
+    id: '1007', title: '蜘蛛侠', name: '彼得·帕克', gender: '先生', height: 177,
+    friends: [ '1001' ], appearsIn: [ 'Avengers' ],
+  },
+  {
+    id: '1008', title: '绿巨人', name: '布鲁斯·班纳', gender: '先生', height: null,
+    friends: [ '1000','1001','1002','2001' ], appearsIn: [ 'Avengers' ],
+  },
+  {
+    id: '1009', title: '万磁王', name: '埃里克·兰谢尔', gender: '先生', height: 188,
+    friends: [  ], appearsIn: [ 'XMen' ],
+  }
 ];
 
 const humanData = {};
@@ -283,60 +80,31 @@ humans.forEach((ship) => {
   humanData[ship.id] = ship;
 });
 
-const droids = [
+const aliens = [
   {
-    id: '2000',
-    name: 'C-3PO',
-    friends: [ '1000', '1002', '1003', '2001' ],
-    appearsIn: [ 'NEWHOPE', 'EMPIRE', 'JEDI' ],
-    primaryFunction: 'Protocol',
+    id: '2001', title: '雷神', name: '索尔·奥丁森', homePlanet: '阿斯加德',
+    friends: [ '1000','1001','1002','1008', ],
+    appearsIn: [ 'Thor', 'Avengers' ]
   },
   {
-    id: '2001',
-    name: 'R2-D2',
-    friends: [ '1000', '1002', '1003' ],
-    appearsIn: [ 'NEWHOPE', 'EMPIRE', 'JEDI' ],
-    primaryFunction: 'Astromech',
-  },
+    id: '2002', title: '卡魔拉', name: '卡魔拉·贞·忽贝莉·本·泰坦', homePlanet: '泽侯贝里族',
+    friends: [ '1006' ],
+    appearsIn: [ 'GuardiansOfTheGalaxy', 'Avengers' ]
+  }
 ];
 
-const droidData = {};
-droids.forEach((ship) => {
-  droidData[ship.id] = ship;
-});
-
-const starships = [
-  {
-    id: '3000',
-    name: 'Millenium Falcon',
-    length: 34.37,
-  },
-  {
-    id: '3001',
-    name: 'X-Wing',
-    length: 12.5,
-  },
-  {
-    id: '3002',
-    name: 'TIE Advanced x1',
-    length: 9.2,
-  },
-  {
-    id: '3003',
-    name: 'Imperial shuttle',
-    length: 20,
-  },
-];
-
-const starshipData = {};
-starships.forEach((ship) => {
-  starshipData[ship.id] = ship;
+const alienData = {};
+aliens.forEach((ship) => {
+  alienData[ship.id] = ship;
 });
 
 var reviews = {
-  'NEWHOPE': [],
-  'EMPIRE': [],
-  'JEDI': []
+  'Avengers': [],
+  'XMen': [],
+  'GuardiansOfTheGalaxy': [],
+  'IronMan': [],
+  'CaptainAmerica': [],
+  'Thor': []
 };
 
 
@@ -345,7 +113,15 @@ var reviews = {
  */
 function getCharacter(id) {
   // Returning a promise just to illustrate GraphQL.js's support.
-  return Promise.resolve(humanData[id] || droidData[id]);
+  return Promise.resolve(humanData[id] || alienData[id]);
+}
+
+function sayHi(name) {
+  if(!name) {
+    name = 'World';
+  }
+
+  return `Hello ${name}`;
 }
 
 /**
@@ -359,12 +135,19 @@ function getFriends(character) {
  * Allows us to fetch the undisputed hero of the Star Wars trilogy, R2-D2.
  */
 function getHero(episode) {
-  if (episode === 'EMPIRE') {
-    // Luke is the hero of Episode V.
+  if (episode === 'Avengers') {
     return humanData['1000'];
-  }
-  // Artoo is the hero otherwise.
-  return droidData['2001'];
+  } else if (episode === 'XMen') {
+    return humanData['1009'];
+  } else if (episode === 'GuardiansOfTheGalaxy') {
+    return humanData['1006'];
+  } else if (episode === 'IronMan') {
+    return humanData['1001'];
+  } else if (episode === 'CaptainAmerica') {
+    return humanData['1000'];
+  } else {
+    alienData['2001']
+  };
 }
 
 /**
@@ -382,14 +165,10 @@ function getHuman(id) {
 }
 
 /**
- * Allows us to query for the droid with the given id.
+ * Allows us to query for the alien with the given id.
  */
-function getDroid(id) {
-  return droidData[id];
-}
-
-function getStarship(id) {
-  return starshipData[id];
+function getAlien(id) {
+  return alienData[id];
 }
 
 function toCursor(str) {
@@ -404,17 +183,16 @@ const resolvers = {
   Query: {
     hero: (root, { episode }) => getHero(episode),
     character: (root, { id }) => getCharacter(id),
+    hi: (root, { name }) => sayHi(name),
     human: (root, { id }) => getHuman(id),
-    droid: (root, { id }) => getDroid(id),
-    starship: (root, { id }) => getStarship(id),
+    alien: (root, { id }) => getAlien(id),
     reviews: (root, { episode }) => getReviews(episode),
     search: (root, { text }) => {
       const re = new RegExp(text, 'i');
 
       const allData = [
         ...humans,
-        ...droids,
-        ...starships,
+        ...aliens,
       ];
 
       return allData.filter((obj) => re.test(obj.name));
@@ -444,19 +222,19 @@ const resolvers = {
       if(humanData[data.id]){
         return info.schema.getType('Human');
       }
-      if(droidData[data.id]){
-        return info.schema.getType('Droid');
+      if(alienData[data.id]){
+        return info.schema.getType('Alien');
       }
       return null;
     },
   },
   Human: {
-    height: ({ height }, { unit }) => {
-      if (unit === 'FOOT') {
-        return height * 3.28084;
+    gender: ({ gender }, { unit }) => {
+      if (unit === 'Sex') {
+        return (gender === '先生') ? '男' : '女';
       }
 
-      return height;
+      return gender;
     },
     friends: ({ friends }) => friends.map(getCharacter),
     friendsConnection: ({ friends }, { first, after }) => {
@@ -478,10 +256,9 @@ const resolvers = {
         totalCount: friends.length
       };
     },
-    starships: ({ starships }) => starships.map(getStarship),
     appearsIn: ({ appearsIn }) => appearsIn,
   },
-  Droid: {
+  Alien: {
     friends: ({ friends }) => friends.map(getCharacter),
     friendsConnection: ({ friends }, { first, after }) => {
       first = first || friends.length;
@@ -514,28 +291,13 @@ const resolvers = {
     node: ({ node }) => node,
     cursor: ({ cursor }) => cursor,
   },
-  Starship: {
-    length: ({ length }, { unit }) => {
-      if (unit === 'FOOT') {
-        return length * 3.28084;
-      }
-
-      return length;
-    },
-    coordinates: () => {
-      return [[1, 2], [3, 4]];
-    }
-  },
   SearchResult: {
     __resolveType(data, context, info){
       if(humanData[data.id]){
         return info.schema.getType('Human');
       }
-      if(droidData[data.id]){
-        return info.schema.getType('Droid');
-      }
-      if(starshipData[data.id]){
-        return info.schema.getType('Starship');
+      if(alienData[data.id]){
+        return info.schema.getType('Alien');
       }
       return null;
     },
